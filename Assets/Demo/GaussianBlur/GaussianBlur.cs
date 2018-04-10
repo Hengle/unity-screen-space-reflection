37c8920e442fb470c1216cda3d10cc14bde13ff8
komietty-lab <komietty@gmail.com>
2018-04-10 21:02:04 +0900

    bugfix on windows src to src blit

--- Assets/Demo/GaussianBlur/GaussianBlur.cs
+++ Assets/Demo/GaussianBlur/GaussianBlur.cs
@@ -21,11 +21,16 @@ public class GaussianBlur : MonoBehaviour
 
     void OnRenderImage(RenderTexture src, RenderTexture dst)
     {
+        var rt = RenderTexture.GetTemporary(src.width, src.height, 0, src.format);
+
         for (int i = 0; i < blurNum; i++)
         {
-            Graphics.Blit(src, mat, 0);
-            Graphics.Blit(src, mat, 1);
+            Graphics.Blit(src, rt, mat, 0);
+            Graphics.Blit(rt, src, mat, 1);
         }
+
         Graphics.Blit(src, dst);
+
+        RenderTexture.ReleaseTemporary(rt);
     }
 }
